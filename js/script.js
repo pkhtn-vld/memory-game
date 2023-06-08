8a08e8ae4633b81cc94b516ac6d6ce20cbe011bb
pkhtn-vld <vlad_w12@mail.ru>
2023-06-08 07:12:29 +0300

    Refactor code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 class Card {
-  constructor(element, cardNumber) {
-    this.element = element;
+  constructor(cardNumber, container, flip) {
+    this.container = container;
     this.cardNumber = cardNumber;
+    this.flip = flip;
+    this.element = this.createElement();
     this.open = false;
     this.success = false;
   }
@@ -30,10 +32,10 @@ class Card {
 
   set success(value) {
     if (value) {
-      this.element.removeEventListener("click", flipCard);
+      this.element.removeEventListener("click", this.handleClick);
       this._success = true;
     } else {
-      this.element.addEventListener("click", flipCard);
+      this.element.addEventListener("click", this.handleClick);
       this._success = false;
     }
   }
@@ -41,18 +43,61 @@ class Card {
   get success() {
     return this._success;
   }
+
+  createElement() {
+    const cardElement = document.createElement("div");
+    cardElement.classList.add("memory-card");
+    cardElement.setAttribute("data-framework", this.cardNumber);
+
+    const backFace = document.createElement("img");
+    backFace.classList.add("back-face");
+    backFace.setAttribute("src", "img/card.jpg");
+    backFace.setAttribute("alt", "Memory Card");
+
+    cardElement.appendChild(backFace);
+    cardElement.addEventListener("click", this.handleClick);
+
+    this.container.appendChild(cardElement);
+    return cardElement;
+  }
+
+  handleClick = () => {
+    this.flip(this.element);
+  };
 }
 
+class AmazingCard extends Card {
+  constructor(cardNumber, container, flip) {
+    super(cardNumber, container, flip);
+    this._cardNumber = cardNumber;
+
+    const imgElement = document.createElement("img");
+    imgElement.classList.add("front-face");
+    imgElement.setAttribute("src", `img/${cardNumber}.svg`);
+    imgElement.setAttribute("alt", cardNumber);
+
+    imgElement.onerror = () => {
+      imgElement.setAttribute("src", "img/default.png");
+      throw new Error(`Ошибка загрузки изображения`);
+    };
+
+    this.element.appendChild(imgElement);
+  }
+}
+
+const memoryGameContainer = document.querySelector("#memory-game-container");
 const cards = document.querySelectorAll(".memory-card");
+const CARD_IMAGES = [ "angular", "css", "html", "js", "nodejs", "php", "react", "vue", ];
+
 let cardsArray = [];
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
 
-function flipCard(card) {
+function flipCard(cardElement) {
   if (lockBoard) return;
 
-  card = cardsArray.find((card) => card.element === this);
+  let card = cardsArray.find((card) => card.element === cardElement);
 
   if (card === firstCard) return;
 
@@ -109,8 +154,7 @@ function shuffleMain(array) {
 }
 
 function addNewGame() {
-  
-  if (cardsArray.every(card => card.open)) {
+  if (cardsArray.every((card) => card.open)) {
     document.querySelector(".new-game").style.display = "block";
     celebrate();
     setTimeout(celebrate, 1000);
@@ -124,10 +168,10 @@ function addNewGame() {
 }
 
 function createCardsArray() {
-  for (const cardElement of cards) {
-    let cardNumber = cardElement.dataset.framework;
-    let card = new Card(cardElement, cardNumber);
-    cardsArray.push(card);
+  for (const cardNumber of CARD_IMAGES) {
+    let card1 = new AmazingCard(cardNumber, memoryGameContainer, flipCard);
+    let card2 = new AmazingCard(cardNumber, memoryGameContainer, flipCard);
+    cardsArray.push(card1, card2);
   }
 
   return cardsArray;
@@ -136,7 +180,6 @@ function createCardsArray() {
 function initializeCards(cardsArray) {
   cardsArray.forEach((card, index) => {
     card.element.style.order = index;
-    card.element.addEventListener("click", flipCard(card));
   });
 }
 
